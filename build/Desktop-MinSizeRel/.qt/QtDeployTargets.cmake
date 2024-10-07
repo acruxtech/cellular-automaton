98a92e19a42c14e6c35be3ac5904dee8e43dbbf6
AcruxTech <acrux_tech@mail.ru>
2024-10-08 02:17:15 +0300

    delete build

--- build/Desktop-MinSizeRel/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cellular-automaton_FILE /home/acruxtech/Documents/!3_study/cpp/qt/cellular-automaton/build/Desktop-MinSizeRel/cellular-automaton)
-set(__QT_DEPLOY_TARGET_cellular-automaton_TYPE EXECUTABLE)
